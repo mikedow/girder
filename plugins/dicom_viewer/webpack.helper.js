49982fda973b4c9fe12bb38ecc64f3cf81776f61
Brian Helba <brian.helba@kitware.com>
2017-05-09 12:39:02 -0400

    Migrate Webpack config's "Rule.loaders" / "Rule.loader" to "Rule.use"
    This is the preferred syntax for Webpack 2.0:
    https://webpack.js.org/configuration/module/#rule-loaders

--- plugins/dicom_viewer/webpack.helper.js
+++ plugins/dicom_viewer/webpack.helper.js
@@ -1,25 +1,30 @@
 module.exports = function (config) {
     config.module.rules.push({
         test: /\.glsl$/,
-        loader: 'shader-loader',
+        use: [
+            'shader-loader'
+        ],
         include: [/node_modules(\/|\\)vtk\.js(\/|\\)/],
     });
     config.module.rules.push({
         test: /\.js$/,
         include: [/node_modules(\/|\\)vtk\.js(\/|\\)/],
-        loaders: [{
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
+        use: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                loader: 'string-replace-loader',
+                query: {
+                    multiple: [
+                        {search: /test\.onlyIfWebGL/g, replace: 'test'}
+                    ]
+                }
             }
-        }, {
-            loader: 'string-replace-loader',
-            query: {
-                multiple: [
-                    {search: /test\.onlyIfWebGL/g, replace: 'test'}
-                ]
-            }
-        }]
+        ]
     });
     return config;
 };
